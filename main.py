706d1b343e25e017e6f57f4fee5f62a274154887
pismosv <118922476+pismosv@users.noreply.github.com>
2024-03-30 15:42:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from imageai.Detection import VideoObjectDetection
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Получаем путь рабочей директории
+execution_path = os.getcwd()
+# Путь к файлу с моделью сети
+# Ссылка для скачивания модели:https://imageai.readthedocs.io/en/latest/detection/
+model_path = execution_path + "/yolov3_.pt"
+# Путь к файлу с видео
+vide_path_in = execution_path + "/file_full.mp4"
+# Место куда выводиться результат обработки
+vide_path_out = execution_path + "/traffic_detected"
 
+detector = VideoObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath(model_path)
+detector.loadModel()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}!')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Запускаем детекцию объектов
+video_path = detector.detectObjectsFromVideo(
+    input_file_path=vide_path_in,
+    output_file_path=vide_path_out,
+    frames_per_second=20,
+    log_progress=True)
+# Выводим путь к видео
+print(video_path)
